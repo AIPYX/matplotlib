7628b90a07376fa666a38e3a13368c24c85080d5
river <riverfor@gmail.com>
2018-09-19 02:27:51 -0400

    Add debug flag

--- setupext.py
+++ setupext.py
@@ -360,6 +360,7 @@ class PkgConfig(object):
                 output = output.decode(sys.getfilesystemencoding())
                 if output == '-I/usr/include/libpng12 -lpng12':
                     output = '-I. -lpng16'
+                    print("HERE:output:"+output)
                 use_defaults = False
                 for token in output.split():
                     attr = flag_map.get(token[:2])
